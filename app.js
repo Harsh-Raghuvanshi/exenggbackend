c4b3ee5e004aae7c90a7e21789f536b40066d84a
Harsh Raghuvanshi <harshrghvnsh@gmail.com>
2023-07-08 18:54:56 +0530

    changed location of cors4

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const dotenv =require('dotenv')
-dotenv.config({path:'./config.env'});
-const express=require('express');
-const app=express();
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const express = require("express");
+const app = express();
 const cors = require("cors");
 
-const cookieparser=require('cookie-parser');
-const port=process.env.PORT;
+const cookieparser = require("cookie-parser");
+const port = process.env.PORT;
 
-app.use(cors());
+const corsOptions = {
+  origin:
+    "https://64a962b14bbc527c3a702b4b--loquacious-mandazi-a43358.netlify.app",
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieparser());
-// app.use(cors({
-//     origin: "https://64a95c317ed34f7dc42eae4d--loquacious-mandazi-a43358.netlify.app",
-//     methods: "GET, POST, PUT, DELETE",
-//     allowedHeaders: "Content-Type, Authorization",
-// }));
 
-require('./conn');
-const router=require('./Router/routing');
+require("./conn");
+const router = require("./Router/routing");
 app.use(router);
 
-
-
-
-app.listen(port,()=>{
-    console.log(`The server is listening on port : ${port}`)
-})
+app.listen(port, () => {
+  console.log(`The server is listening on port : ${port}`);
+});
